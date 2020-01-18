896be444be904a00d065d8141a653afc0a6e8f3a
prayas2409 <prayas.hemraj.pagade@gmail.com>
2020-01-18 19:06:11 +0530

    Routes modified

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import card from './components/card';
+import home from "./components/home"
 import headerbar from "./components/headerbar"
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
                 <Route path="/" component={headerbar} exact={false} />
-                <Route path="/card" component={card} exact={true} />
+                <Route path="/home" component={home} exact={true} />
                 
         </div>
     </BrowserRouter>
