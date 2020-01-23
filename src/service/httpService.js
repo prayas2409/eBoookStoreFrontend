dedc39f12e3f9be30a994216c4ed837e7138edb5
prayas2409 <prayas.hemraj.pagade@gmail.com>
2020-01-23 13:08:18 +0530

    backend url updated in httpservice

--- src/service/httpService.js
+++ src/service/httpService.js
@@ -1,9 +1,9 @@
 const axios = require('axios').default;
 // TODO: const url = "htt "
+const URL="http://172.31.18.122:3000/"
 
 export function getMethod(data) {
     console.log(data.path);
 
-    return axios.get('http://localhost:3000/' + data.path);
-// comment
+    return axios.get( URL+ data.path);
 }
